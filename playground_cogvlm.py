5d1003049a68ce41581e0b979cfa2d8a007d0794
raoulritter <raoulflorisritter@gmail.com>
2024-05-22 21:04:05 +0200

    Update playground_cogvlm.py to imageurl

--- playground_cogvlm.py
+++ playground_cogvlm.py
@@ -145,8 +145,8 @@ tokenizer = LlamaTokenizer.from_pretrained("lmsys/vicuna-7b-v1.5")
 
 # PROCESS IMAGE
 # image_path = '/home/jwiers/POPE/data/val2014/COCO_val2014_000000000042.jpg'
-image_url = Image.open(requests.get('http://images.cocodataset.org/val2014/COCO_val2014_000000000042.jpg', stream=True).raw).convert('RGB')
-image = Image.open(image_url)
+image = Image.open(requests.get('http://images.cocodataset.org/val2014/COCO_val2014_000000000042.jpg', stream=True).raw).convert('RGB')
+# image = Image.open(image_url)
 
 # image_tensor = preprocess_pipeline(image).unsqueeze(0).to(DEVICE)
 text_emb, processed_image = _get_text_embedding(model, tokenizer, "a photo of a cat", DEVICE, image)
