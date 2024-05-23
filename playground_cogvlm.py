38bbb5cad999323da8c3a87f0abbb3ae7c69857b
raoulritter <raoulflorisritter@gmail.com>
2024-05-23 15:11:41 +0200

    update with inspec

--- playground_cogvlm.py
+++ playground_cogvlm.py
@@ -100,7 +100,7 @@ def _get_text_embedding(model, tokenizer, query, device, image):
         print(inspect.getsource(model.generate))
 
         print(inspect.getsource(model.build_conversation_input_ids))
-        
+
         print("text_embedding shape: ",text_embedding.shape)
 
         text_embedding = text_embedding[:, inputs['input_ids'].shape[1]:]
@@ -162,7 +162,8 @@ tokenizer = LlamaTokenizer.from_pretrained("lmsys/vicuna-7b-v1.5")
 
 # PROCESS IMAGE
 # image_path = '/home/jwiers/POPE/data/val2014/COCO_val2014_000000000042.jpg'
-image = Image.open(requests.get('http://images.cocodataset.org/val2014/COCO_val2014_000000000042.jpg', stream=True).raw).convert('RGB')
+# image = Image.open(requests.get('http://images.cocodataset.org/val2014/COCO_val2014_000000000042.jpg', stream=True).raw).convert('RGB')
+image = None
 # image = Image.open(image_url)
 
 # image_tensor = preprocess_pipeline(image).unsqueeze(0).to(DEVICE)
